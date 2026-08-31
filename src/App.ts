eb5d15acb3bc36bb5448edde1b70ff5367836d22
Isayas Adhanom <isayasadhanom@gmail.com>
2026-08-31 09:44:38 -0500

    pre-class commit

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,5 @@
 /* Lecture 4
- *  CS 4388/ CS 5388, Fall 2026, Texas State University
+ * CS 4388/ CS 5388, Fall 2026, Texas State University
  * Instructor: Isayas Berhe Adhanom <isayas@txstate.edu> * 
  * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International. Adapted from course materials by Evan Suma Rosenberg.
  */ 
